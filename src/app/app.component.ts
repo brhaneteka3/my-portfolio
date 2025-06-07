62a0f381b572436d37dc6a81cba328ff90537671
Brhane Bahrishum Teka <brhanebt@Brhanes-MacBook-Pro.local>
2025-06-07 16:47:16 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle'
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSlideToggleModule, MatToolbarModule, 
+    MatIconModule, MatButtonModule, MatMenuModule, MatTabsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'my-ang19-portfolio-app';
+  title = 'Welcome to my Portfolio!';
 }
